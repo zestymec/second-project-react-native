9e072b852b56a75b3798ef015a6318b5d707d8fc
zestymec <m.umer394aziz@gmail.com>
2026-04-09 11:43:13 +0500

    Flat cards component updating by adding more content

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import Flatcards from './components/Flatcards'
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>App</Text>
-      <Flatcards/>
-
+      <ScrollView>
+        <Text >App</Text>
+        <Flatcards />
+      </ScrollView>
     </SafeAreaView>
   )
 }
--- components/Flatcards.tsx
+++ components/Flatcards.tsx
@@ -1,12 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+
 export default function Flatcards() {
   return (
     <View>
-      <Text>Flatcards</Text>
+      <Text style={styles.headingtext}>Blue</Text>
+      <View>
+        <View>
+            <Text style={styles.red}>Red</Text>
+        </View>
+        <View>
+            <Text style={styles.red}>pink</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headingtext:{
+        color:'blue',
+        fontSize:24,
+        fontWeight:'bold',
+        padding:8,
+    }
+    ,
+    red:{
+        color:'red',
+        fontSize:24,
+        fontWeight:'bold',
+        padding:8,
+    }
+    ,pink:{
+        color:'pink',
+        fontSize:24,
+        fontWeight:'bold',
+        padding:8,
+    }
+})
