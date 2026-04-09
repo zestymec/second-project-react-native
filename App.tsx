80b222de6aae98e1a7b38f7ff813b91efb1162ee
zestymec <m.umer394aziz@gmail.com>
2026-04-09 10:55:32 +0500

    flat card component created and imported

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
-
+import Flatcards from './components/Flatcards'
 const App = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
+      <Flatcards/>
+
     </SafeAreaView>
   )
 }
